dfbba17a5741a190ee983702d3515153ac3517ce
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-08-28 08:54:32 +0500

    Create P1.sql

---
+++ Basic /P1.sql
@@ -0,0 +1,11 @@
+-- built in functions aggregate functions
+select count(*) from EMPLOYEES; -- count(*) == count all 
+select count(*)as total_employees from EMPLOYEES; -- as changes the col name in the output table
+select count(*)as total_employees, MANAGER_ID from EMPLOYEES group by MANAGER_ID;
+select distinct manager_id from EMPLOYEES; -- doing comparision if different or not
+select manager_id from EMPLOYEES GROUP by manager_id; -- grouping the same managers ID
+select sum(salary) as Total_Salary from EMPLOYEES;
+select sum(salary) as Total_Salary from EMPLOYEES where manager_id = 101;
+select min(salary) as minimum_salary from EMPLOYEES;
+select max(salary) as minimum_salary from EMPLOYEES;
+select avg(salary) as Avg_salary from EMPLOYEES;
