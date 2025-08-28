5a014a8665be88b737fe5e208db99126e28247b7
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-08-28 10:25:03 +0500

    Create P5.sql

---
+++ Basic /P5.sql
@@ -0,0 +1,12 @@
+-- date and time functions
+select add_months ('16-sep-2004',2) from dual; -- adds the number of months in the given month
+select MONTHS_BETWEEN('10-dec-2010','10-jan-2009') from dual; -- return the number of months in between 2 months (bigger one, smaller one) else will return -ve val
+select next_day('4-nov-2005','wednesday') from dual; -- when is wed comming after the given date
+select last_day('1-oct-2005') from dual; -- returns the last day of the month
+select new_time('6-aug-2023','ISL','EST') from dual; -- not working due to time zone issue
+select new_time('6-aug-2023','PST','EST') from dual; -- not working due to time zone issue
+
+-- conversion functions
+select to_char(sysdate,'DD-MM-YY') from dual; 
+select to_char(sysdate,'Day') from dual;
+SELECT * FROM EMPLOYEES WHERE to_char(hire_date,'Day')= 'Wednesday';
