51c6945153b7078cf2806be165aee6fe2a1a016d
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-12-03 18:54:53 +0500

    Update Q.sql

--- Transaction /Q.sql
+++ Transaction /Q.sql
@@ -11,3 +11,25 @@ ROLLBACK TO sp1;
 SET TRANSACTION NAME 'salary_update';
 SET AUTOCOMMIT ON;
 SET AUTOCOMMIT OFF;
+
+SET AUTOCOMMIT ON;
+INSERT INTO worker VALUES (3, 'FAST-NU', 5000); -- auto-committed
+SET AUTOCOMMIT OFF;
+
+--locking
+UPDATE worker SET salary = 6000 WHERE worker_id = 1;
+UPDATE worker SET salary = 7000 WHERE worker_id = 1; --will wait till commit or rollback
+
+SET TRANSACTION NAME 'test_transaction';
+
+INSERT INTO worker VALUES (2, 'Erum', 5500);
+SAVEPOINT sp1;
+
+UPDATE worker SET salary = 6000 WHERE worker_name = 'Erum';
+SAVEPOINT sp2;
+
+ROLLBACK TO SAVEPOINT sp1; -- salary returns to 5500
+
+COMMIT;
+
+
